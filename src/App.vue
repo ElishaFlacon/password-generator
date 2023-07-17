931f53f841a39c0d1e4b57959d5577abc893244b
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-17 11:58:07 +0500

    init vue + vuetify password generator

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app class="bg">
+        <v-main>
+            <MainPage />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+
+
+export default {
+    name: 'App',
+
+    components: {
+        MainPage,
+    },
+}
+</script>
+
+<style src="./App.css"/>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-container>
+        <v-responsive class="mx-auto text-center" max-width="500">
+
+            <v-responsive class="text-h4">
+                ГЕНЕРАТОР ПАРОЛЯ v.1.0.0
+            </v-responsive>
+
+            <v-col>
+                <v-col class="flex" max-width="500">
+                    <v-text-field label="Пароль" variant="solo-filled" v-model="password" />
+                    <v-select label="Выберете размер пароля" :items="[4, 6, 8, 10, 12, 14, 16, 18, 20]" v-model="size" />
+                </v-col>
+                <v-btn @click="generation">Сгенерировать пароль</v-btn>
+            </v-col>
+        </v-responsive>
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    name: 'MainPage',
+
+    data: () => ({
+        password: '',
+        size: 10,
+    }),
+
+    methods: {
+        generation() {
+            let result = '';
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+
+            for (let i = 0; i < this.size; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+
+            this.password = result;
+        }
+    }
+
+
+}
+</script>
